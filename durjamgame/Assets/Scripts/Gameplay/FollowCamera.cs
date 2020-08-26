45353dfb76db2871e8a1b4eeb09c6f33fe82031b
richmondvan <evan.ty.zheng@gmail.com>
2020-08-26 12:28:50 -0400

    - Locked camera to forward-most player

---
+++ durjamgame/Assets/Scripts/Gameplay/FollowCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine.Utility;
+using Cinemachine;
+using System;
+using UnityEngine.Serialization;
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+
+namespace Platformer.Gameplay
+{
+    // Custom implementation of Cinemachine Virtual Camera
+    public class FollowCamera : CinemachineVirtualCamera
+    {
+        // Update is called once per frame
+        protected override void Update()
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            GameObject maxPlayer = players[0];
+            Debug.Log(players.Length);
+            foreach (var player in players)
+            {
+                if (maxPlayer is null)
+                {
+                    maxPlayer = player;
+                }
+                else
+                {
+                    if (player.transform.position.x > maxPlayer.transform.position.x)
+                    {
+                        maxPlayer = player;
+                    }
+                }
+            }
+            m_Follow = maxPlayer.transform;
+            m_LookAt = maxPlayer.transform;
+            base.Update();
+        }
+    }
+}
+
