8073b0fccc08409cd4bf4e13e319a36aecfbbb59
richmondvan <evan.ty.zheng@gmail.com>
2020-08-28 12:41:06 -0400

    Fixed parallax

--- durjamgame/Assets/Scripts/View/ParallaxLayer.cs
+++ durjamgame/Assets/Scripts/View/ParallaxLayer.cs
@@ -11,18 +11,22 @@ namespace Platformer.View
         /// <summary>
         /// Movement of the layer is scaled by this value.
         /// </summary>
-        public Vector3 movementScale = Vector3.one;
+        // public Vector3 movementScale = Vector3.one;
+        public float xMovementScale;
+
+        private float originalCameraX;
 
         Transform _camera;
 
         void Awake()
         {
             _camera = Camera.main.transform;
+            originalCameraX = _camera.position.x;
         }
 
         void LateUpdate()
         {
-            transform.position = Vector3.Scale(_camera.position, movementScale);
+            transform.position = new Vector3((_camera.position.x - originalCameraX) * xMovementScale, transform.position.y, transform.position.z);
         }
 
     }
