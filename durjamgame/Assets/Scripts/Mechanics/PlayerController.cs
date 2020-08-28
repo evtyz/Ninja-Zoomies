02844db710b358d6b29a0d7452098afbd96859b3
richmondvan <evan.ty.zheng@gmail.com>
2020-08-27 22:02:49 -0400

    Fixed double jump force

--- durjamgame/Assets/Scripts/Mechanics/PlayerController.cs
+++ durjamgame/Assets/Scripts/Mechanics/PlayerController.cs
@@ -195,6 +195,7 @@ public class PlayerController : MonoBehaviour
 		
 			// Add a vertical force to the player.
 			m_Grounded = false;
+			m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce + (float)bonusForce * 10));
 		}
 
